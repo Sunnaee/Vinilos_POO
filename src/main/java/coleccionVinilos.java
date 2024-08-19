9b4f46b5ca811f4ade1619b6944126f08dd5014b
Sabrina <s.lopez15@ufromail.cl>
2024-08-18 23:25:30 -0400

    busqueda de artista implementada

--- src/main/java/coleccionVinilos.java
+++ src/main/java/coleccionVinilos.java
@@ -3,6 +3,11 @@ public class coleccionVinilos {
         String[][] vinilos = new String[100][3];
 
         vinilos = agregarVinilo(vinilos, "Jihyo", "Zone", "2023");
+        vinilos = agregarVinilo(vinilos, "Whee in", "Redd", "2021");
+        vinilos = agregarVinilo(vinilos, "NMIXX", "expérgo", "2023");
+        vinilos = agregarVinilo(vinilos, "NMIXX", "Fe3O4: BREAK", "2024");
+        vinilos = agregarVinilo(vinilos, "Billlie", "the Billage of perception: chapter three", "2023");
+        vinilos = agregarVinilo(vinilos, "NewJeans", "Supernatural", "2024");
 
         System.out.println("Espacio máximo colección: "+vinilos.length);
 
@@ -10,29 +15,34 @@ public class coleccionVinilos {
 
         printDisponibles(vinilos);
 
-        //String artista = nombre artista;
-        //System.out.println("Buscando artista: "+artista);
-        //mostrarBusqArtista(vinilos,artista);
-        //buscarArtista(vinilos,artista);
+        String artista = "Jihyo";
+        System.out.println("Buscando artista: "+artista);
+        mostrarBusqArtista(vinilos,artista);
+        buscarArtista(vinilos,artista);
+
         mostrarColeccion(vinilos);
     }
 
     //agregar vinilos
     public static String [][] agregarVinilo(String[][] matriz, String artista, String album, String anio){
-        int fila = cantRegistrados(matriz);
-        matriz[fila][0] = artista;
-        matriz[fila][1] = album;
-        matriz[fila][2] = anio;
+        if(cantRegistrados(matriz) != 100) {
+            int fila = cantRegistrados(matriz);
+            matriz[fila][0] = artista;
+            matriz[fila][1] = album;
+            matriz[fila][2] = anio;
+            return matriz;
+        } else {
+            System.out.println("No quedan espacios disponibles para agregar vinilos.");
+        }
         return matriz;
     }
 
     //calcular registrados
     public static int cantRegistrados(String[][] matriz){
-        int registrados = 0;
         for(int i=0;i<100;i++){
             for(int j=0;j<3;j++){
-                if (matriz[i][j] == null){
-                    return registrados = i;
+                if(matriz[i][j] == null){
+                    return i;
                 }
             }
         }
@@ -62,5 +72,20 @@ public class coleccionVinilos {
     }
 
     //buscar artista bool
+    public static boolean buscarArtista(String[][] matriz, String artista){
+        for(int i=0;i<100;i++) {
+            if (matriz[i][0].equals(artista)) {
+                return true;
+            }
+        }
+        return false;
+    }
     //mostrar resultados artista
+    public static void mostrarBusqArtista(String[][] matriz, String artista){
+        if(buscarArtista(matriz, artista)){
+            System.out.println("El artista "+artista+" se encuentra en la colección.");
+        } else{
+            System.out.println("El artista "+artista+" NO se encuentra en la colección.");
+        }
+    }
 }
